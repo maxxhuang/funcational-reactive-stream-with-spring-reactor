0f70e6ef299f5c42ba91e0b3281228d6fb148f77
maxxhuang <maxxlife@gmail.com>
2018-10-01 09:34:05 +0800

    add webflux dependency for spring-reactor example

---
+++ reactive-spring-reactor/src/main/java/tenam/learning/reactivespringreactor/devicecontroller/DeviceControllerApplication.java
@@ -0,0 +1,14 @@
+package tenam.learning.reactivespringreactor.devicecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DeviceControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceControllerApplication.class, args);
+    }
+
+}
