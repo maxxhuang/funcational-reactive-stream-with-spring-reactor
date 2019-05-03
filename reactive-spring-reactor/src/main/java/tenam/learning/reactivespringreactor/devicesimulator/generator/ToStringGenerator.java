d2ea03e00a7f81d3dfd8498bf37b422cf1331e55
maxxhuang <maxxlife@gmail.com>
2019-05-03 18:42:52 +0800

    remove license declaration

--- reactive-spring-reactor/src/main/java/tenam/learning/reactivespringreactor/devicesimulator/generator/ToStringGenerator.java
+++ reactive-spring-reactor/src/main/java/tenam/learning/reactivespringreactor/devicesimulator/generator/ToStringGenerator.java
@@ -1,13 +1,3 @@
-/**
- * Copyright 2013 Ruckus Wireless, Inc. All rights reserved.
- *
- * RUCKUS WIRELESS, INC. CONFIDENTIAL - 
- * This is an unpublished, proprietary work of Ruckus Wireless, Inc., and is 
- * fully protected under copyright and trade secret laws. You may not view, 
- * use, disclose, copy, or distribute this file or any information contained 
- * herein except pursuant to a valid license from Ruckus. 
- */
- 
 package tenam.learning.reactivespringreactor.devicesimulator.generator;
 
 public class ToStringGenerator<F> extends GeneratorConverter<F, String> {
